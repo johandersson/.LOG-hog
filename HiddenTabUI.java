3a1d8a16344d881a5ff0e1ed89d84c27742db1f8
Johan Andersson <johan.andersson@lakemedelsverket.se>
2025-10-14 14:30:40 +0200

    Choose first item by default

---
+++ HiddenTabUI.java
@@ -0,0 +1,27 @@
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+import java.awt.*;
+
+public final class HiddenTabUI extends BasicTabbedPaneUI {
+    @Override
+    protected int calculateTabAreaHeight(int tabPlacement, int runCount, int maxTabHeight) {
+        return 0; // reserve no space for tabs
+    }
+
+    @Override
+    protected void paintTabBackground(Graphics g, int tabPlacement, int tabIndex,
+                                      int x, int y, int w, int h, boolean isSelected) {
+        // no-op: don't paint tab background
+    }
+
+    @Override
+    protected void paintTabBorder(Graphics g, int tabPlacement, int tabIndex,
+                                  int x, int y, int w, int h, boolean isSelected) {
+        // no-op: don't paint tab border
+    }
+
+    @Override
+    protected void paintContentBorder(Graphics g, int tabPlacement, int selectedIndex) {
+        // default content border painting is OK; if you want none, leave empty
+        super.paintContentBorder(g, tabPlacement, selectedIndex);
+    }
+}
