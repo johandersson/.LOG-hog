8fbc531b19daf2adeb4348981f6d4a6a84b56696
Johan Andersson <johan.andersson@lakemedelsverket.se>
2025-10-21 13:25:05 +0200

    gpl

--- AboutPanel.java
+++ AboutPanel.java
@@ -75,13 +75,16 @@ public class AboutPanel extends JPanel {
 
 
     private String defaultLicenseMissingText() {
-        return "Copyright 2025 Johan Andersson\n" +
-                "\n" +
-                "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n" +
-                "\n" +
-                "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n" +
-                "\n" +
-                "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n" +
-                "\n";
+       //gpl3 text
+        return "GNU GENERAL PUBLIC LICENSE\n" +
+                "Version 3, 29 June 2007\n\n" +
+                "Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>\n" +
+                "Everyone is permitted to copy and distribute verbatim copies\n" +
+                "of this license document, but changing it is not allowed.\n\n" +
+                "Preamble\n\n" +
+                "The GNU General Public License is a free, copyleft license for\n" +
+                "software and other kinds of works.\n\n" +
+                "[Full license text not found. Please refer to https://www.gnu.org/licenses/gpl-3.0.en.html for the complete license.]\n";
+
     }
 }
