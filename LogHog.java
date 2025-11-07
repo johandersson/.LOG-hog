808c2fb3d03b3470cb2aeb34b8c7aaef4a8c8c0e
Johan Andersson <johan.andersson@tutamail.com>
2025-11-07 20:06:18 +0100

    Quick entry from tray

--- LogHog.java
+++ LogHog.java
@@ -21,49 +21,7 @@ public class LogHog {
             System.out.println("System tray not supported!");
             return;
         }
-
-        // Create a minimal 1x1 transparent image
-        Image image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-
-        // Create popup menu
-        PopupMenu popup = new PopupMenu();
-
-        // Create menu items
-        MenuItem openItem = new MenuItem("Open");
-        MenuItem exitItem = new MenuItem("Exit");
-
-        // Add items to popup menu
-        popup.add(openItem);
-        popup.addSeparator();
-        popup.add(exitItem);
-
-        // Create tray icon with tooltip
-        TrayIcon trayIcon = new TrayIcon(image, "My Tray App", popup);
-        trayIcon.setImageAutoSize(true);
-
-        // Add click listener to tray icon
-        trayIcon.addActionListener(e -> {
-            System.out.println("Tray icon clicked!");
-        });
-
-        // Add action listeners to menu items
-        openItem.addActionListener(e -> {
-            System.out.println("Open selected");
-            // You can show a window or perform another action here
-        });
-
-        exitItem.addActionListener(e -> {
-            System.out.println("Exiting...");
-            System.exit(0);
-        });
-
-        // Add tray icon to system tray
-        try {
-            SystemTray.getSystemTray().add(trayIcon);
-        } catch (AWTException e) {
-            System.out.println("TrayIcon could not be added.");
-        }
-
         SwingUtilities.invokeLater(() -> LogTextEditor.main(args));
     }
+
 }
