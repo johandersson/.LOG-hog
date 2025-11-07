3eb42e644888cf5abf0abc687186d2cc9a3d469c
Johan Andersson <johan.andersson@lakemedelsverket.se>
2025-11-07 12:30:24 +0100

    Quick entry from tray

--- LogHog.java
+++ LogHog.java
@@ -1,27 +1,18 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 
-public class LogHog {
-    public static void main(String[] args) {
-        try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
+void main(String[] args) {
+    try {
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Windows".equals(info.getName())) {
+                UIManager.setLookAndFeel(info.getClassName());
+                break;
             }
-        } catch (Exception ignored) {}
-
-        // Let the OS draw the title bar and buttons (native chrome)
-        JFrame.setDefaultLookAndFeelDecorated(false);
-
-        // Check if system tray is supported
-        if (!SystemTray.isSupported()) {
-            System.out.println("System tray not supported!");
-            return;
         }
-        SwingUtilities.invokeLater(() -> LogTextEditor.main(args));
+    } catch (Exception ignored) {
     }
 
+    // Let the OS draw the title bar and buttons (native chrome)
+    JFrame.setDefaultLookAndFeelDecorated(false);
+
+    SwingUtilities.invokeLater(() -> LogTextEditor.main(args));
 }
