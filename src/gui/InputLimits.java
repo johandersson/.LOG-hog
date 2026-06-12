406b861ec7161b5cc6411ea97bcc18d53922b073
Johan Andersson <johan.andersson@tutamail.com>
2026-06-12 15:00:31 +0200

    Update InputLimits.java

--- src/gui/InputLimits.java
+++ src/gui/InputLimits.java
@@ -3,10 +3,14 @@ package gui;
 public final class InputLimits {
     // Maximum characters allowed in a single log entry (~64 KiB)
     public static final int ENTRY_MAX_CHARS = 65536;
+
     // Generic max for auxiliary text fields (URLs, paths, etc.)
     public static final int FIELD_MAX_CHARS = 8192;
+
     // Display text when inserting links
     public static final int DISPLAY_TEXT_MAX = 2048;
+
+    public static final int PASSWORD_MAX_CHARS = 256;
 
     private InputLimits() { }
 }
